41759fe2085d9e34d32cdba1db253117daf54570
nazzzarr@gmail.com <nazzzarr@gmail.com>
2021-07-04 20:01:09 +0300

    slider finish

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: true,
+    dots: false,
+    // adaptiveHeight: true,
+    // slidesToShow: 2,
+    // slidesToScroll: 2,
+    speed: 1000,
+    easeing: 'linear',
+    infinite: true,
+    autoplay: true
+  });
+});
